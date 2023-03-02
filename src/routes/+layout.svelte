fd6ae75682c854ff74d39aa6aad46d7b6dc93068
Sebastian Geiger <geigersr@clarkson.edu>
2023-03-02 16:11:43 -0500

    try to get global css to work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,31 @@
 <script>
-    import '../global.css';
+    import 'global.css';
   </script>
   <nav>
     <a href="/">Home</a>
     <a href="/about">About</a>
   </nav>
-  <slot />
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <p>&copy;<span class="numbers">2023</span> Sebastian Geiger</p>
+  </footer>
+
+  <style>
+    nav, footer {
+      display: flex;
+      justify-content: space-evenly;
+      padding: .5em;
+      background-color:honeydew;
+      color: seagreen;
+    }
+    main {
+      grid-row: 2;
+    }
+    footer {
+    grid-row: 3;
+    position: sticky;
+    bottom: 0;
+  }
+  </style>
