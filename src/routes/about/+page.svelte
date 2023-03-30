af2bbc6317374ae172ca5315dfb04f02b680e352
Sebastian Geiger <geigersr@clarkson.edu>
2023-03-29 20:25:14 -0400

    changed about page text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,5 @@
 <h1>About</h1>
-<p>Basic page description</p>
+<p>I chose this blog because I am a big fan of hockey and I wanted to chose an API that interested me. 
+    This blog allowed me to use SvelteKit to display information about NHL teams and learn a bit more about the NHL.
+    I hope that you learn as much as I do or even more! Enjoy!
+</p>
