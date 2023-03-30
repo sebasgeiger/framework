48c13d5f3e0a35f6cd8cecf88051acb59e7a06d2
Sebastian Geiger <geigersr@clarkson.edu>
2023-03-30 11:40:57 -0400

    got rid of comments

--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -17,12 +17,3 @@ export async function load({ fetch,params }) {
   }
 } 
 
-//import { posts } from '../data.js';
-
-//export function load({ params }) {
-  //const post = posts.find((post) => post.slug === params.slug);
-
-//  return {
-//    post
-//  }
-//}
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -15,14 +15,6 @@
     {/each}
 </article>
 
-<!-- <script>
-    export let data;
-  </script>
-  
-  <h1>{data.post.title}</h1>
-  <p>Article #{data.post.slug}</p>
-  <p>{data.post.content}</p> -->
-
 <style>
     article {
         display: grid;
