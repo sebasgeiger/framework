dbafa2cf64e8ad50098c6da9dec5836a0ea1ce23
Sebastian Geiger <geigersr@clarkson.edu>
2023-04-11 13:58:06 -0400

    deployment attempt

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+/* import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config} 
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -10,4 +10,18 @@ const config = {
 	}
 };
 
-export default config;
+export default config; */
+
+import adapter from '@sveltejs/adapter-static';
+ 
+const dev = process.argv.includes('dev');
+ 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/your-repo-name',
+    }
+  }
+};
